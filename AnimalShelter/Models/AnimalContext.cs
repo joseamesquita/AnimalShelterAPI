47132867fe27107112bf1073d9009040f6e9b4ae
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-13 08:02:11 -0700

    create and update database for first object/animal

--- AnimalShelter/Models/AnimalContext.cs
+++ AnimalShelter/Models/AnimalContext.cs
@@ -10,5 +10,14 @@ namespace AnimalShelter.Models
     }
 
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+          .HasData(
+              new Animal { AnimalId = 1, Name = "Bingo", Species = "Chihuahua", Age = 2, Gender = "Male" }
+
+          );
+    }
   }
 }
