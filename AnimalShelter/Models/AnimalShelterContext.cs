71af0a22353435387b29dddc38b58926429808e2
Jose Amesquita <joseamesquita11@gmail.com>
2020-04-15 02:09:05 -0700

    seed the database with Animal entries in Animal Shelter Context

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -10,5 +10,16 @@ namespace AnimalShelter.Models
     }
 
     public DbSet<Animal> Animals { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+          .HasData(
+              new Animal { AnimalId = 1, Name = "Max", Species = "German-Shepherd", Age = 5, Gender = "Male" },
+              new Animal { AnimalId = 2, Name = "Lucy", Species = "Showshoe", Age = 1, Gender = "Female" },
+              new Animal { AnimalId = 3, Name = "Bingo", Species = "Persian Cat", Age = 2, Gender = "Male" },
+              new Animal { AnimalId = 4, Name = "Chico", Species = "Bulldog", Age = 3, Gender = "Male" },
+              new Animal { AnimalId = 5, Name = "Camila", Species = "Chihuahua", Age = 4, Gender = "Female" }
+          );
+    }
   }
 }
